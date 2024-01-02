e3d3375151823e5fb8cd68092fc21d00eadb3a34
dev-kozaka <78633477+dev-kozaka@users.noreply.github.com>
2024-01-02 20:06:16 +0900

    Revert "deviceとメールを追加"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  namespace :api do
-    mount_devise_token_auth_for 'User', at: 'auth'
-  end
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :books, only: [:index]
 end
